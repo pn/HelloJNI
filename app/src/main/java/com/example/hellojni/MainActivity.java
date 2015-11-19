339d2296e51aeb46d22cb723dc0af32e418a9a23
Paweł Nadolski <pawel.nadolski@gmail.com>
2015-11-19 11:21:13 +0100

    Define and load MyLib library

--- app/src/main/java/com/example/hellojni/MainActivity.java
+++ app/src/main/java/com/example/hellojni/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    static {
+        System.loadLibrary("MyLib");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
