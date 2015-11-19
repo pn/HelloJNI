2779be0a3311f3d6dd5a973d2de5b383081a4ae2
Paweł Nadolski <pawel.nadolski@gmail.com>
2015-11-19 11:23:00 +0100

    Display text from jni

--- app/src/main/java/com/example/hellojni/MainActivity.java
+++ app/src/main/java/com/example/hellojni/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.hellojni;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.hello_text);
+        textView.setText(helloFromJni());
     }
 
     public native String helloFromJni();
